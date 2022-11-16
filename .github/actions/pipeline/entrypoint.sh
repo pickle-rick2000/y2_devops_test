1aa984423ea2460b91218aa348f94b85773f12b8
Jhony <jhonatansela1@gmail.com>
2022-11-16 20:51:24 +0200

    downgraded

--- .github/actions/pipeline/entrypoint.sh
+++ .github/actions/pipeline/entrypoint.sh
@@ -3,8 +3,9 @@
 echo "Hello from brnach: ${GITHUB_REF##*/}"
 
  apt update
- apt install -y docker docker.io
+ apt install -y docker=17 docker.io
 
+echo ${DOCKER_USERNAME} {DOCKER_PASSWORD}
 docker login -u ${DOCKER_USERNAME} -p {DOCKER_PASSWORD}
-docker build -t jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA} .
-docker push jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA}
+# docker build -t jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA} .
+# docker push jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA}
