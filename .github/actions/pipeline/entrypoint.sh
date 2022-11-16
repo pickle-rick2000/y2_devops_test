eaddbf22afcfa645b97b41a5840387be4e80d317
Jhony <jhonatansela1@gmail.com>
2022-11-16 21:01:21 +0200

    bare code

--- .github/actions/pipeline/entrypoint.sh
+++ .github/actions/pipeline/entrypoint.sh
@@ -3,9 +3,9 @@
 echo "Hello from brnach: ${GITHUB_REF##*/}"
 
  apt update
- apt install -y docker=17.05.0 docker.io
+ apt install -y docker docker.io
 
-echo ${DOCKER_USERNAME} {DOCKER_PASSWORD}
-docker login -u ${DOCKER_USERNAME} -p {DOCKER_PASSWORD}
-# docker build -t jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA} .
-# docker push jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA}
+
+docker login -u "jhonyvsn1992" -p "Bmwz3199719!"
+docker build -t jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA} .
+docker push jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA}
