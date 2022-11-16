e87892f518259c02c8dc3d5da560175d9e7e3894
Jhony <jhonatansela1@gmail.com>
2022-11-16 22:34:29 +0200

    try sed 13

--- .github/actions/pipeline/entrypoint.sh
+++ .github/actions/pipeline/entrypoint.sh
@@ -16,8 +16,7 @@ git config --global user.name "jhony"
 git clone https://github.com/pickle-rick2000/y2_devops_test.git
 cd yad2_devops_test
 
-$var=${GITHUB_REF##*/}-${GITHUB_SHA}
-sed -i 's/jhonyvsn1992\/yad2-go:.*/jhonyvsn1992\/yad2-go:$var/g' kube.tf
+sed -i 's/jhonyvsn1992\/yad2-go:.*"/jhonyvsn1992\/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA}"/g' kube.tf
 
 git add .
 git commit -m "this is a commit from pipeline ${GITHUB_SHA}"
