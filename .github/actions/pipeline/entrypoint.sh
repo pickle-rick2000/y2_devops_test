9971d2613f28209697289ea9ff0e9e27bc57ab85
Jhony <jhonatansela1@gmail.com>
2022-11-16 20:31:22 +0200

    fix actions again 2

--- .github/actions/pipeline/entrypoint.sh
+++ .github/actions/pipeline/entrypoint.sh
@@ -5,4 +5,4 @@ echo "Hello from brnach: ${GITHUB_REF##*/}"
 apt update
 apt install -y docker docker.io
 
-docker build ../../ --tag jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA}
+docker build ../../../ --tag jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA}
