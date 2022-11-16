075dd7a87a79bf729151e90501988d01c5686252
Jhony <jhonatansela1@gmail.com>
2022-11-16 22:15:48 +0200

    try sed 9

--- .github/actions/pipeline/entrypoint.sh
+++ .github/actions/pipeline/entrypoint.sh
@@ -11,12 +11,13 @@
 
 git config --global --add safe.directory /github/workspace
 git config --global user.email "jhonatansela1@gmail.com"
-git config --global user.name "jhonyy"
+git config --global user.name "jhony"
 
 git clone https://github.com/pickle-rick2000/y2_devops_test.git
 cd yad2_devops_test
 
-sed -i 's/image = "jhonyvsn1992\/yad2-go:.*"/image = "jhonyvsn1992\/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA}"/g' kube.tf
+$var=${GITHUB_REF##*/}-${GITHUB_SHA}
+sed -i 's/image = "jhonyvsn1992\/yad2-go:.*"/image = "jhonyvsn1992\/yad2-go:$var"/g' kube.tf
 
 git add .
 git commit -m "this is a commit from pipeline ${GITHUB_SHA}"
