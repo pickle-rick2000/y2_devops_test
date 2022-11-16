a4a9e44d1919be79f7cfa019363777d3f9b2341c
Jhony <jhonatansela1@gmail.com>
2022-11-16 21:57:39 +0200

    try sed 3

--- .github/actions/pipeline/entrypoint.sh
+++ .github/actions/pipeline/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#  apt update
-#  apt install -y docker docker.io
+ apt update
+ apt install -y docker docker.io git
 
 
 # docker login -u "jhonyvsn1992" -p "Bmwz3199719!"
@@ -9,5 +9,12 @@
 # docker build -t jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA} .
 # docker push jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA}
 
+git clone https://github.com/pickle-rick2000/y2_devops_test.git
+cd yad2_devops_test
 
 sed -i 's/image = "jhonyvsn1992\/yad2-go:.*"/image = "jhonyvsn1992\/yad2-go:hello/g' kube.tf
+
+git add .
+git commit -m "this is a commit from pipeline ${GITHUB_SHA}"
+git push
+
