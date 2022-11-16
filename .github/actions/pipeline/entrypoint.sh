a201c4f83f7398da177b123f6e16cd4adb5fe82f
Jhony <jhonatansela1@gmail.com>
2022-11-16 20:40:06 +0200

    it worked, now push

--- .github/actions/pipeline/entrypoint.sh
+++ .github/actions/pipeline/entrypoint.sh
@@ -6,3 +6,4 @@ echo "Hello from brnach: ${GITHUB_REF##*/}"
  apt install -y docker docker.io
 
 docker build -t jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA} .
+docker push jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA}
