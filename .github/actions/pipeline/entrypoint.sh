c1ea383115c06708d6cf03dd383ae12d5f44fd65
Jhony <jhonatansela1@gmail.com>
2022-11-16 21:44:58 +0200

    testind to deploy

--- .github/actions/pipeline/entrypoint.sh
+++ .github/actions/pipeline/entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
- apt update
- apt install -y docker docker.io
+
+#  apt update
+#  apt install -y docker docker.io
 
 
-docker login -u "jhonyvsn1992" -p "Bmwz3199719!"
+# docker login -u "jhonyvsn1992" -p "Bmwz3199719!"
 
-docker build -t jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA} .
-docker push jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA}
+# docker build -t jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA} .
+# docker push jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA}
 
+echo hello world > hello.txt
+
+ls -l
+cat hello.txt
