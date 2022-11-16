d4ebf9de42b22abde8e3f2de834c28745dcd9e30
Jhony <jhonatansela1@gmail.com>
2022-11-16 20:04:03 +0200

    fix actions now i see

--- .github/actions/pipeline/entrypoint.sh
+++ .github/actions/pipeline/entrypoint.sh
@@ -2,21 +2,4 @@
 
 echo "Hello from brnach: ${GITHUB_REF##*/}"
 
-
-name: Build Docker Image For Go
-
-on:
-  push:
-    branches: [ "master" ]
-
-
-jobs:
-
-  build:
-
-    runs-on: ubuntu-latest
-
-    steps:
-    - uses: actions/checkout@v3
-    - name: Build the Docker image
-      run: docker build . --file ../../Dockerfile --tag jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA}
+docker build . --file ../../Dockerfile --tag jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA}
