92da1d988a70fb5824a8c6d13653aa5164727a17
Jhony <jhonatansela1@gmail.com>
2022-11-16 22:51:16 +0200

    test docker

--- .github/actions/pipeline/entrypoint.sh
+++ .github/actions/pipeline/entrypoint.sh
@@ -8,5 +8,13 @@ docker login -u "jhonyvsn1992" -p "Bmwz3199719!"
 
 docker build -t jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA} .
 docker build -t jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-latest .
+
+if "post" -eq "post"
+then
+    echo "good!!!!!!!!!!!!!!!!!!!!!"
+else
+    echo "failed!!!!!!!!!!!!!!!!!!!!"
+fi
+
 docker push jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA}
 docker push jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-latest
