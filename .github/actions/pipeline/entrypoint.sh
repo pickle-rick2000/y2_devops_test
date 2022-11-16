c79ed0fb7dfce0bc53918071d4248bc0a5c3fe5a
Jhony <jhonatansela1@gmail.com>
2022-11-16 20:37:34 +0200

    hope this one works

--- .github/actions/pipeline/entrypoint.sh
+++ .github/actions/pipeline/entrypoint.sh
@@ -2,11 +2,7 @@
 
 echo "Hello from brnach: ${GITHUB_REF##*/}"
 
-ls -l
-pwd
-cat Dockerfile
+ apt update
+ apt install -y docker docker.io
 
-# apt update
-# apt install -y docker docker.io
-
-# docker build . --tag jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA}
+docker build -t jhonyvsn1992/yad2-go:${GITHUB_REF##*/}-${GITHUB_SHA} .
